6bbc961312835614b45b421029b2d05bdc28b226
Jin Mac jinctmac@gmail.com <jinctmac@gmail.com>
2021-08-17 13:15:42 +0100

    day 27 todolist changes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,41 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    # we need to find the task to show it by id
+    @task = Task.find(params[:id])
+    # raise
+    # to see what is inside the value of params, which id is
+  end
+
+  def new
+    @task = Task.new
+    # then in the respective view, we use the helper form_for to make the form
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+
+    # this takes us to the task show page
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    # need to find the task to show it by id again
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  # we need strong params to save the data in the db
+  # this tasks_params method is needed to bypass the forbidden attributes error
+  # because it is a security method to prevent users from updating the form
+  # and use malicious code in the params
+  # we can replace params[:task] in create, etc
+  # with this method call to bypass the security
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'restaurants', to: 'restaurants#index'
+  # read
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: 'task' # here we set as task
+  # this ensures that we can make the link_to task['title'] and task_path work
+  # which takes the path tasks/task['id']
+  # create
+  post 'tasks', to: 'tasks#create'
+  # edit
+  # step1: get the html form
+  # step2: patch the params to save the task
+  # remember, as we are editing a single task instance
+  # we need to specify the id
+  get 'tasks/:id/edit', to: 'tasks#edit'
 end
